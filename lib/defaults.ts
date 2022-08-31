4ff3c43bd9163d9019c3eb140f2114d13ca85666
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-31 22:36:22 +0200

    Default values for forms and list of supported social media

---
+++ lib/defaults.ts
@@ -0,0 +1,13 @@
+export const supportedSocialList = ['facebook', 'twitter', 'instagram'];
+
+export const defaultSignInValues = {
+	username_or_email: '',
+	password: '',
+};
+
+export const defaultSignUpValues = {
+	email: '',
+	username: '',
+	password: '',
+	confirm_password: '',
+};
