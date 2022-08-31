1583f2502f8693b12b81818ced4842743a5e84bc
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-31 22:03:49 +0200

    Adding replacing contents with MiniDrawer component

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,19 +1,11 @@
-import { Link } from '@mui/material';
 import styles from '../styles/Nav.module.scss';
+import MiniDrawer from './MiniDrawer';
 
 const Nav = () => {
 	return (
-		<div className={styles.nav}>
-			<div className={styles.left}>
-				<Link href='/'>
-					<h1>Social Media Dashboard</h1>
-				</Link>
-			</div>
-			<div className={styles.right}>
-				<div className={styles.account}>Acc</div>
-				<button className={styles.toggle_mode}>Toggle D/L Mode</button>
-			</div>
-		</div>
+		<nav className={styles.nav}>
+			<MiniDrawer />
+		</nav>
 	);
 };
 
