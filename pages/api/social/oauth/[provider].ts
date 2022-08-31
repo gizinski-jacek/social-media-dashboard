17946e037a28711826ada01cf3ba72dbdfa3705e
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-31 22:22:22 +0200

    Social media oauth route using passport.

---
+++ pages/api/social/oauth/[provider].ts
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from 'next';
+import nc from 'next-connect';
+import { passport } from '../../../../lib/middleware/passport';
+
+const handler = nc<NextApiRequest, NextApiResponse>({
+	onError: (err, req, res, next) => {
+		console.error(err.stack);
+		res.status(500).end('Something broke!');
+	},
+	onNoMatch: (req, res) => {
+		res.status(404).end('Page not found');
+	},
+}).get(async (req, res, next) => {
+	const { provider } = req.query as { provider: string };
+	if (!provider) {
+		return res.status(404).end('Provider not supported');
+	}
+	passport.authenticate(provider, { session: false })(req, res, next);
+});
+
+export default handler;
