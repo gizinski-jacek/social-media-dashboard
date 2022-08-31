79fa03ce5306ff23e579e4151529cdd4c2fb2cd7
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-31 22:06:10 +0200

    Importing and implementing hooks.
    Implementing hooks for next-auth's session, selected social media and theme.
    Using server side props to get user's session.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,40 @@
+import { getSession, GetSessionParams, SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
+import Head from 'next/head';
 import Nav from './Nav';
-import Footer from './Footer';
 import styles from '../styles/Layout.module.scss';
+import { SocialContextProvider } from '../hooks/SocialProvider';
+import { ThemeContextProvider } from '../hooks/ThemeProvider';
 
-const Layout = ({ children }: React.PropsWithChildren) => {
+interface Props {
+	children: React.ReactNode;
+	session: Session;
+}
+
+const Layout = ({ children, session }: Props) => {
 	return (
-		<div className={styles.container}>
-			<Nav />
-			<main className={styles.main}>{children}</main>
-			<Footer />
-		</div>
+		<SessionProvider session={session}>
+			<SocialContextProvider>
+				<ThemeContextProvider>
+					<Head>
+						<title>Social Media Dashboard</title>
+						<meta name='description' content='Social Media Dashboard' />
+						<link rel='icon' href='/favicon.ico' />
+					</Head>
+					<Nav />
+					<main className={styles.main}>{children}</main>
+				</ThemeContextProvider>
+			</SocialContextProvider>
+		</SessionProvider>
 	);
+};
+
+export const getServerSideProps = async (context: GetSessionParams) => {
+	return {
+		props: {
+			session: await getSession(context),
+		},
+	};
 };
 
 export default Layout;
