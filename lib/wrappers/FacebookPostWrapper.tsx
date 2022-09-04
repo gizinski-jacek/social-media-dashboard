12d003be97ee6ffa03907c755baabd4b5566f6b0
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-04 20:03:59 +0200

    Updating with more data.

--- lib/wrappers/FacebookPostWrapper.tsx
+++ lib/wrappers/FacebookPostWrapper.tsx
@@ -1,5 +1,6 @@
-import { Box } from '@mui/material';
-import axios from 'axios';
+import { Card, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import Image from 'next/image';
 import { FacebookPost } from '../../types/myTypes';
 
 interface Props {
@@ -8,9 +9,21 @@ interface Props {
 
 const FacebookPostWrapper = ({ data }: Props) => {
 	return (
-		<Box alignItems={'center'} id={data.id}>
-			<Box>{data.message}</Box>
-			<Box>
+		<Card
+			sx={{
+				flex: 1,
+				m: 2,
+				mb: 0,
+				minHeight: 80,
+				height: 'fit-content',
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'space-between',
+			}}
+			id={data.id}
+		>
+			<Typography variant='caption' m={1}>
+				{'Posted: '}
 				{new Date(data.created_time).toLocaleDateString(undefined, {
 					year: 'numeric',
 					month: 'numeric',
@@ -18,8 +31,56 @@ const FacebookPostWrapper = ({ data }: Props) => {
 					hour: 'numeric',
 					minute: 'numeric',
 				})}
-			</Box>
-		</Box>
+			</Typography>
+			<Typography variant='h5' m={1}>
+				{data.message}
+			</Typography>
+			{data.attachments?.data.map((d) => (
+				<Box key={d.target.id}>
+					<Box>
+						{d.subattachments?.data.map((s, index) => (
+							<Image
+								key={s.target.id}
+								src={s.media.image.src}
+								width={s.media.image.width}
+								height={s.media.image.height}
+								alt={`Attachment number ${index}`}
+							/>
+						))}
+						{d.subattachments ? null : (
+							<Image
+								src={d.media.image.src}
+								width={d.media.image.width}
+								height={d.media.image.height}
+								alt={d.title}
+							/>
+						)}
+					</Box>
+					<Typography variant='h6'>{d.description}</Typography>
+				</Box>
+			))}
+			{data.comments?.data.map((c) => (
+				<Card key={c.id} id={c.id}>
+					Comment:
+					<Typography variant='caption' m={1}>
+						{'Posted: '}
+						{new Date(c.created_time).toLocaleDateString(undefined, {
+							year: 'numeric',
+							month: 'numeric',
+							day: 'numeric',
+							hour: 'numeric',
+							minute: 'numeric',
+						})}
+						<Typography variant='h5' m={1}>
+							{c.message}
+						</Typography>
+						<Typography variant='h6' m={1} id={c.from.id}>
+							{c.from.name}
+						</Typography>
+					</Typography>
+				</Card>
+			))}
+		</Card>
 	);
 };
 
