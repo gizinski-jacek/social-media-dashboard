24011757879c5addc1347177fbb2ccd8ebbeabc2
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-04 19:59:59 +0200

    Adding facebook hostname URL for next/image component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		domains: ['scontent-waw1-1.xx.fbcdn.net', 'static.xx.fbcdn.net'],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
