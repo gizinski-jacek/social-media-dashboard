7af40d1937730d69e7cfc57ea84b1f948c76456e
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-05 18:55:30 +0200

    Adding withAuth middleware.

--- pages/api/social-oauth/[provider].ts
+++ pages/api/social-oauth/[provider].ts
@@ -1,12 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
+import { withAuth } from '../../../lib/middleware/withAuth';
 import { passport } from '../../../lib/passport/passport';
 
 const scopes = {
 	facebook: [], // Leave empty, use profileFields in strategy instead
 	twitter: ['users.read ', 'tweet.read ', 'offline.access'],
-	instagram: ['user_profile', 'user_media'],
+	instagram: ['user_profile', 'user_media', 'user_posts', 'instagram_basic'],
 };
 
 const handler = nc<NextApiRequest, NextApiResponse>({
@@ -17,16 +18,17 @@ const handler = nc<NextApiRequest, NextApiResponse>({
 	onNoMatch: (req, res) => {
 		return res.status(404).end('Page not found');
 	},
-}).get(async (req, res, next) => {
-	const { provider } = req.query as { provider: string };
-	if (!provider) {
-		return res.status(404).end('Provider not supported');
-	}
-	passport.authenticate(provider, { session: false, scope: scopes[provider] })(
-		req,
-		res,
-		next
-	);
-});
+})
+	.use(withAuth)
+	.get(async (req, res, next) => {
+		const { provider } = req.query as { provider: string };
+		if (!provider) {
+			return res.status(404).end('Provider not supported');
+		}
+		passport.authenticate(provider, {
+			session: false,
+			scope: scopes[provider],
+		})(req, res, next);
+	});
 
 export default handler;
