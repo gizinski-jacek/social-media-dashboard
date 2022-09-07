162499bc6d2fcf6e559e487c6730360145473565
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-07 17:02:30 +0200

    Refactoring, styling, adding state and handlers.
    Readding Image component. Using workaround for dynamic domains by replacing
    image uri to one of allowed domains in next config.
    Adding useEffect and ref to set state used for determining whether post
    content is long enough to justify rendering 'Show More' button.
    Adding state and handlers for expanding shown content and comments.
    Doing some general styling and layout changes.

--- components/socials/wrappers/FacebookPostWrapper.tsx
+++ components/socials/wrappers/FacebookPostWrapper.tsx
@@ -1,6 +1,8 @@
-import { Card, Typography } from '@mui/material';
+import { Button, Card } from '@mui/material';
 import { Box } from '@mui/system';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
 import { FacebookPost } from '../../../types/myTypes';
 
 interface Props {
@@ -8,79 +10,181 @@ interface Props {
 }
 
 const FacebookPostWrapper = ({ data }: Props) => {
+	const [contentIsTall, setContentIsTall] = useState(false);
+	const [showMoreContent, setShowMoreContent] = useState(false);
+	const [showComments, setShowComments] = useState(false);
+	const ref = useRef<HTMLDivElement>(null);
+
+	const handleToggleShow = () => {
+		setShowMoreContent((prevState) => !prevState);
+		setShowComments(false);
+	};
+
+	const handleToggleComments = () => {
+		setShowComments((prevState) => !prevState);
+	};
+
+	useEffect(() => {
+		setContentIsTall(ref.current?.clientHeight > 200);
+	}, []);
+
 	return (
 		<Card
 			sx={{
-				flex: 1,
-				m: 2,
-				mb: 0,
-				minHeight: 80,
-				height: 'fit-content',
+				maxHeight: contentIsTall ? (showMoreContent ? 'auto' : 200) : 200,
+				width: 300,
+				m: 1,
 				display: 'flex',
 				flexDirection: 'column',
 				justifyContent: 'space-between',
+				position: 'relative',
+				border: '1px solid blue',
+				a: {
+					color: 'teal',
+				},
 			}}
 			id={data.id}
 		>
-			<Typography variant='caption' m={1}>
-				{'Posted: '}
-				{new Date(data.created_time).toLocaleDateString(undefined, {
-					year: 'numeric',
-					month: 'numeric',
-					day: 'numeric',
-					hour: 'numeric',
-					minute: 'numeric',
-				})}
-			</Typography>
-			<Typography variant='h5' m={1}>
-				{data.message}
-			</Typography>
-			{data.attachments?.data.map((d) => (
-				<Box key={d.target.id}>
-					{/* {Removing images rendering until I find a way to accept images from dynamic FB domains} */}
-					{/* <Box>
-						{d.subattachments?.data.map((s, index) => (
-							<Image
-								key={s.target.id}
-								src={d.media.image.src}
-								width={s.media.image.width}
-								height={s.media.image.height}
-								alt={`Attachment number ${index}`}
-							/>
-						))}
-						{d.subattachments ? null : (
-							<Image
-								src={d.media.image.src}
-								width={d.media.image.width}
-								height={d.media.image.height}
-								alt={d.title}
-							/>
-						)}
-					</Box> */}
-					<Typography variant='h6'>{d.description}</Typography>
-				</Box>
-			))}
-			{data.comments?.data.map((c) => (
-				<Card key={c.id} id={c.id}>
-					Comment:
-					<Typography variant='caption' m={1}>
+			<Box
+				sx={{
+					m: 1,
+					flex: 1,
+					display: 'flex',
+					flexDirection: 'column',
+				}}
+			>
+				<Box>
+					<Link
+						href={`https://www.facebook.com/profile.php?id=${data.from.id}`}
+					>
+						{data.from.name}
+					</Link>
+					<h6>
 						{'Posted: '}
-						{new Date(c.created_time).toLocaleDateString(undefined, {
+						{new Date(data.created_time).toLocaleDateString(undefined, {
 							year: 'numeric',
 							month: 'numeric',
 							day: 'numeric',
 							hour: 'numeric',
 							minute: 'numeric',
 						})}
-						<Typography variant='h5' m={1}>
-							{c.message}
-						</Typography>
-						<Typography variant='h6' m={1} id={c.from.id}>
-							{c.from.name}
-						</Typography>
-					</Typography>
-				</Card>
-			))}
+					</h6>
+				</Box>
+				<Box
+					ref={ref}
+					sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}
+				>
+					<Box
+						sx={{
+							flex: 1,
+							maxWidth: '100%',
+							overflowWrap: 'break-word',
+							mb: 'auto',
+						}}
+					>
+						<p>{data.message}</p>
+					</Box>
+					{data.attachments ? (
+						<Box>
+							{data.attachments?.data.map((d) => (
+								<Box key={d.target.id}>
+									{/* Hacky solution for dynamic domains issue with Image component */}
+									{/* Replacing part of uri to specific allowed domain in config */}
+									<Box>
+										{d.subattachments?.data.map((s, index) => (
+											<Link key={s.target.id} href={s.target.url}>
+												<Image
+													src={d.media.image.src.replace(
+														/.+?(?=.fbcdn.net)/,
+														'https://scontent.fpoz4-1.fna'
+													)}
+													width={s.media.image.width}
+													height={s.media.image.height}
+													alt={s.description || `Attachment ${s.type}`}
+													placeholder='blur'
+													blurDataURL={d.media.image.src.replace(
+														/.+?(?=.fbcdn.net)/,
+														'https://scontent.fpoz4-1.fna'
+													)}
+												/>
+											</Link>
+										))}
+										{d.subattachments ? null : (
+											<Link href={d.target.url}>
+												<Image
+													src={d.media.image.src.replace(
+														/.+?(?=.fbcdn.net)/,
+														'https://scontent.fpoz4-1.fna'
+													)}
+													width={d.media.image.width}
+													height={d.media.image.height}
+													alt={d.description || `Attachment ${d.type}`}
+												/>
+											</Link>
+										)}
+									</Box>
+								</Box>
+							))}
+						</Box>
+					) : null}
+					<Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+						<Button
+							type='button'
+							variant={showComments ? 'contained' : 'outlined'}
+							onClick={handleToggleComments}
+						>
+							Comments
+						</Button>
+						<Button variant='outlined' component='a' href={data.permalink_url}>
+							Go To Post
+						</Button>
+					</Box>
+				</Box>
+			</Box>
+			{showComments ? (
+				<Box>
+					{data.comments?.data.map((c) => (
+						<Card key={c.id} id={c.id} sx={{ m: 1, p: 1 }}>
+							<Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+								<Link
+									href={`https://www.facebook.com/profile.php?id=${c.from.id}`}
+								>
+									{c.from.name}
+								</Link>
+								<h6>
+									{new Date(c.created_time).toLocaleDateString(undefined, {
+										year: 'numeric',
+										month: 'numeric',
+										day: 'numeric',
+										hour: 'numeric',
+										minute: 'numeric',
+									})}
+								</h6>
+							</Box>
+							<Box>
+								<p>{c.message}</p>
+							</Box>
+						</Card>
+					))}
+				</Box>
+			) : null}
+			{contentIsTall ? (
+				<Button
+					type='button'
+					onClick={handleToggleShow}
+					variant='contained'
+					sx={{
+						opacity: 0.5,
+						position: showMoreContent ? 'static' : 'absolute',
+						bottom: 0,
+						width: '100%',
+						zIndex: 5,
+						textAlign: 'center',
+					}}
+				>
+					{showMoreContent ? 'Show Less' : 'Show More'}
+				</Button>
+			) : null}
 		</Card>
 	);
 };
