1e1c8d5567729d7d1a9ba59411b8904dd31d0456
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-07 17:07:41 +0200

    Updating sign up route.
    Implementing next-connect.
    Adding trycatch block.
    Using yupValidator to check fields for errors and returning them.

--- pages/api/account/signup.ts
+++ pages/api/account/signup.ts
@@ -5,11 +5,8 @@ import yupValidation from '../../../lib/yupValidation';
 import connectMongo from '../../../lib/mongoose';
 import User from '../../../models/user';
 import bcryptjs from 'bcryptjs';
-import {
-	SignInData,
-	SignUpData,
-	UserSignInModel,
-} from '../../../types/myTypes';
+import { SignUpData, UserModel } from '../../../types/myTypes';
+import nc from 'next-connect';
 
 const userSignUpValidationSchema: Yup.SchemaOf<SignUpData> = Yup.object().shape(
 	{
@@ -19,7 +16,7 @@ const userSignUpValidationSchema: Yup.SchemaOf<SignUpData> = Yup.object().shape(
 			.max(32, 'Email max length 32 characters')
 			.email('Invalid email format')
 			.test('email-taken', 'Email is already taken', async (value) => {
-				const account_list: SignInData[] = await User.find({
+				const account_list: UserModel[] = await User.find({
 					email: value,
 				}).exec();
 				return account_list.length === 0;
@@ -30,7 +27,7 @@ const userSignUpValidationSchema: Yup.SchemaOf<SignUpData> = Yup.object().shape(
 			.min(4, 'Username min length 4 characters')
 			.max(32, 'Username max length 32 characters')
 			.test('username-taken', 'Username is already taken', async (value) => {
-				const account_list: UserSignInModel[] = await User.find({
+				const account_list: UserModel[] = await User.find({
 					username: value,
 				}).exec();
 				return account_list.length === 0;
@@ -41,30 +38,48 @@ const userSignUpValidationSchema: Yup.SchemaOf<SignUpData> = Yup.object().shape(
 			.min(4, 'Password min length 4 characters')
 			.max(32, 'Password max length 32 characters')
 			.required('Password is required'),
-		confirm_password: Yup.string()
+		repeat_password: Yup.string()
 			.trim()
-			.min(4, 'Confirm password min length 4 characters')
-			.max(32, 'Confirm password max length 32 characters')
+			.min(4, 'Repeat password min length 4 characters')
+			.max(32, 'Repeat password max length 32 characters')
 			.oneOf([Yup.ref('password')], 'Passwords do not match')
-			.required('Confirm password is required'),
+			.required('Repeat password is required'),
 	}
 );
 
-export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse
-) {
-	const { email, username, password } = req.body;
-	await connectMongo();
-	const hashedPassword = await bcryptjs.hash(password, 5);
-	const newUser = new User({
-		email: email,
-		username: username,
-		password: hashedPassword,
-	});
-	const savedUser = await newUser.save();
-	if (!savedUser) {
-		return res.status(404).json('Error creating user');
+const handler = nc<NextApiRequest, NextApiResponse>({
+	onError: (err, req, res, next) => {
+		console.error(err.stack);
+		return res.status(500).end('Something broke!');
+	},
+	onNoMatch: (req, res) => {
+		return res.status(404).end('Page not found');
+	},
+}).post(async (req, res, next) => {
+	try {
+		const { errors } = await yupValidation(
+			userSignUpValidationSchema,
+			req.body
+		);
+		if (errors) {
+			return res.status(422).json(errors);
+		}
+		const { email, username, password } = req.body;
+		await connectMongo();
+		const hashedPassword = await bcryptjs.hash(password, 5);
+		const newUser = new User({
+			email: email,
+			username: username,
+			password: hashedPassword,
+		});
+		const savedUser = await newUser.save();
+		if (!savedUser) {
+			return res.status(404).json('Error creating user');
+		}
+		return res.status(200).json({ success: true });
+	} catch (error) {
+		return res.status(404).json('Unknown error');
 	}
-	return res.status(200).json({ success: true });
-}
+});
+
+export default handler;
